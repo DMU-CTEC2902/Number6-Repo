e001f28b5c2b247833e0d7a32a25cc4884415ad4
p17158198 <p17158198@GWHXX653WIN014W>
2019-02-15 14:55:58 +0000

    created a form

--- Article Website/Article Website/Form1.Designer.cs
+++ Article Website/Article Website/Form1.Designer.cs
@@ -28,24 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.Music = new System.Windows.Forms.Label();
+            this.Exercise = new System.Windows.Forms.Label();
+            this.Sports = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // Music
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 71);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.Music.AutoSize = true;
+            this.Music.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Music.Location = new System.Drawing.Point(96, 38);
+            this.Music.Name = "Music";
+            this.Music.Size = new System.Drawing.Size(81, 29);
+            this.Music.TabIndex = 0;
+            this.Music.Text = "Music";
+            // 
+            // Exercise
+            // 
+            this.Exercise.AutoSize = true;
+            this.Exercise.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exercise.Location = new System.Drawing.Point(350, 38);
+            this.Exercise.Name = "Exercise";
+            this.Exercise.Size = new System.Drawing.Size(114, 29);
+            this.Exercise.TabIndex = 1;
+            this.Exercise.Text = "Exercise";
+            // 
+            // Sports
+            // 
+            this.Sports.AutoSize = true;
+            this.Sports.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sports.Location = new System.Drawing.Point(222, 38);
+            this.Sports.Name = "Sports";
+            this.Sports.Size = new System.Drawing.Size(89, 29);
+            this.Sports.TabIndex = 2;
+            this.Sports.Text = "Sports";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Sports);
+            this.Controls.Add(this.Exercise);
+            this.Controls.Add(this.Music);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +80,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Music;
+        private System.Windows.Forms.Label Exercise;
+        private System.Windows.Forms.Label Sports;
     }
 }
 
